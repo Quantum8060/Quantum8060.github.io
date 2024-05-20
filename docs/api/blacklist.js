713c05da8dea499ff7281158eb0c4aed8af6f56e
yotuki-tuyu <yotuki.008@gmail.com>
2024-05-20 22:33:20 +0900

    set

---
+++ docs/api/blacklist.js
@@ -0,0 +1,8 @@
+export default function (req, res) {
+  fetch('data.json')
+    .then(response => response.json())
+    .then(data => {
+      const user = data.blacklist.find(blacklist => blacklist.id === req.params.id);
+      res.json(user);
+    });
+}
--- pages/api/blacklist.js
+++
@@ -1,8 +0,0 @@
-export default function(req, res) {
-  fetch('data.json')
-    .then(response => response.json())
-    .then(data => {
-      const user = data.blacklist.find(blacklist => blacklist.id === req.params);
-      res.json(user);
-    });
-}
